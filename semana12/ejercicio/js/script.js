0acff3dd8bc88dc01b4a38c0c72bd6354ec5ffac
Yanis Ramirez <yanisramirez@iMac-4.local>
2015-12-12 12:05:45 -0400

    initial commit

---
+++ semana12/ejercicio/js/script.js
@@ -0,0 +1,13 @@
+
+
+$( "button" ).click(function(){
+  $( ".button" ).animate({ "left": "-=100px" }, "slow" );
+});
+
+
+
+$( "button" ).click(function() {
+  $( "div" ).fadeOut( "slow" );
+  $( "div" ).fadeOut( "slow" );
+});
+
